b2dd116d2e3fbce94ac071aa662b3d4ec7b7cfad
jeikobu__ <github@jeikobu.net>
2018-08-15 13:41:44 +0200

    ongoing development

--- src/main/java/net/jeikobu/jbase/config/AbstractGlobalConfig.java
+++ src/main/java/net/jeikobu/jbase/config/AbstractGlobalConfig.java
@@ -1,8 +1,14 @@
 package net.jeikobu.jbase.config;
 
+import sx.blah.discord.handle.obj.IGuild;
+
 import java.util.Locale;
 
-public interface IGlobalConfig {
+public abstract class AbstractGlobalConfig {
+    public AbstractGlobalConfig(IGuild guild) {
+
+    }
+
     public abstract String getDefaultCommandPrefix();
     public abstract void   setDefaultCommandPrefix(String prefix);
 
