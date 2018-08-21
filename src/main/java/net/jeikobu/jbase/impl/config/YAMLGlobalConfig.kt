511bc3b89f96f85bca82da2f537d7a700220ddd4
jeikobu__ <github@jeikobu.net>
2018-08-21 22:24:47 +0200

    changed default cfg4j environment

--- src/main/java/net/jeikobu/jbase/impl/config/YAMLGlobalConfig.kt
+++ src/main/java/net/jeikobu/jbase/impl/config/YAMLGlobalConfig.kt
@@ -3,6 +3,7 @@ package net.jeikobu.jbase.impl.config
 import net.jeikobu.jbase.config.IGlobalConfig
 import org.cfg4j.provider.ConfigurationProvider
 import org.cfg4j.provider.ConfigurationProviderBuilder
+import org.cfg4j.source.context.environment.ImmutableEnvironment
 import org.cfg4j.source.context.filesprovider.ConfigFilesProvider
 import org.cfg4j.source.files.FilesConfigurationSource
 import org.cfg4j.source.reload.strategy.PeriodicalReloadStrategy
@@ -17,10 +18,12 @@ class YAMLGlobalConfig : IGlobalConfig {
         val configFilesProvider = ConfigFilesProvider {mutableListOf(Paths.get("global.config.yml"))}
         val configSource = FilesConfigurationSource(configFilesProvider)
         val reloadStrategy = PeriodicalReloadStrategy(5, TimeUnit.SECONDS)
+        val environment = ImmutableEnvironment("./")
 
         config = ConfigurationProviderBuilder()
                 .withConfigurationSource(configSource)
                 .withReloadStrategy(reloadStrategy)
+                .withEnvironment(environment)
                 .build()
     }
 
