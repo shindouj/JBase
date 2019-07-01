b95e416d131dfacfe0d9b5ce86b3fad0c15be542
Jakub Strzyżewski <shindouj@users.noreply.github.com>
2019-07-02 01:21:38 +0200

    fixed members visibility (#5)

--- src/main/java/net/jeikobu/jbase/AbstractBot.kt
+++ src/main/java/net/jeikobu/jbase/AbstractBot.kt
@@ -2,16 +2,18 @@ package net.jeikobu.jbase
 
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
+import net.jeikobu.jbase.command.AbstractCommand
 import net.jeikobu.jbase.command.CommandManager
 import net.jeikobu.jbase.config.AbstractConfigManager
 import net.jeikobu.jbase.impl.commands.ChangeLocaleCommand
 import net.jeikobu.jbase.impl.commands.ChangePrefixCommand
 import java.util.*
+import kotlin.reflect.KClass
 
 abstract class AbstractBot(clientBuilder: JDABuilder, configManager: AbstractConfigManager) {
-    private val defaultCommands = Arrays.asList(ChangeLocaleCommand::class, ChangePrefixCommand::class)
-    private val commandManager = CommandManager(configManager)
-    private val client: JDA
+    protected val defaultCommands: MutableList<KClass<out AbstractCommand>> = Arrays.asList(ChangeLocaleCommand::class, ChangePrefixCommand::class)
+    protected val commandManager = CommandManager(configManager)
+    protected val client: JDA
 
     init {
         if (configManager.globalConfig.useDefaultCommands) {
